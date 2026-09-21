7a319d012a720148230382cf30d7fd2353a64870
trungtx92 <31231452+trungtx92@users.noreply.github.com>
2026-09-21 10:03:30 +1000

    DSS-001: update provider

--- Infrastructure/CloudSql/provider.tf
+++ Infrastructure/CloudSql/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.10"
   backend "gcs" {
-    bucket = "${var.project_id}-tfstate"
+    bucket = "fictional-lab-dev-tfstate"
     prefix = "cloudsql"
   }
   required_providers {
