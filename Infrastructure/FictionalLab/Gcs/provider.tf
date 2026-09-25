3672b0a19fe5cc154a863e579ae7b2d2b704e3cd
trungtx92 <31231452+trungtx92@users.noreply.github.com>
2026-09-26 07:07:44 +1000

    DSS-002: update backend

--- Infrastructure/FictionalLab/Gcs/provider.tf
+++ Infrastructure/FictionalLab/Gcs/provider.tf
@@ -1,5 +1,9 @@
 terraform {
   required_version = ">= 1.10"
+  backend "gcs" {
+    bucket = "fictional-lab-tfstate"
+    prefix = "fictional-lab/gcs"
+  }
   required_providers {
     google = {
       source  = "hashicorp/google"
